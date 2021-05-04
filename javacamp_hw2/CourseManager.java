cbb79674b449750a408cbf25831662066498ed53
Tevhid YÜKSEL <tevyuksel@gmail.com>
2021-05-04 20:58:11 +0300

    Homework Week 2

---
+++ javacamp_hw2/CourseManager.java
@@ -0,0 +1,13 @@
+package exam;
+
+public class CourseManager {
+	
+	public void courseStatus(Course course) {
+		System.out.println("Kurs baþarýyla tamamlandý!");	
+	}
+	
+	public void courseCertificate(Course course) {
+		System.out.println("Bu kursu bitirerek sertifika kazandýnýz!");
+	}
+
+}
